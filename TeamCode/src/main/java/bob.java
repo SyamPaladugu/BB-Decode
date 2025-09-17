393f0e071d1f33f69fbfe40b4ccbff04f7003e72
Syam Paladugu <syampaladugu123@gmail.com>
2025-09-17 19:19:33 -0400

    ok

--- TeamCode/src/main/java/bob.java
+++ TeamCode/src/main/java/bob.java
@@ -27,7 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -86,11 +86,11 @@ import java.util.concurrent.TimeUnit;
  */
 
 @TeleOp(name="Omni Drive To AprilTag", group = "Concept")
-@Disabled
-public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
+
+public class bob extends LinearOpMode
 {
     // Adjust these numbers to suit your robot.
-    final double DESIRED_DISTANCE = 25.0; //  this is how close the camera should get to the target (inches)
+    final double DESIRED_DISTANCE = 12.0; //  this is how close the camera should get to the target (inches)
 
     //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
     //  applied to the drive motors to correct the error.
@@ -127,10 +127,10 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
         // Initialize the hardware variables. Note that the strings used here as parameters
         // to 'get' must match the names assigned during the robot configuration.
         // step (using the FTC Robot Controller app on the phone).
-        leftFrontDrive  = hardwareMap.get(DcMotor.class, "lfm");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "rfm");
-        leftBackDrive  = hardwareMap.get(DcMotor.class, "lbm");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "rbm");
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "leftfront_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rightfront_drive");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "leftback_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rightback_drive");
 
         // To drive forward, most robots need the motor on one side to be reversed, because the axles point in opposite directions.
         // When run, this OpMode should start both motors driving forward. So adjust these two lines based on your first test drive.
@@ -227,28 +227,31 @@ public class RobotAutoDriveToAprilTagOmni extends LinearOpMode
      */
     public void moveRobot(double x, double y, double yaw) {
         // Calculate wheel powers.
-        double leftFrontPower    =  x -y -yaw;
-        double rightFrontPower   =  x +y +yaw;
-        double leftBackPower     =  x +y -yaw;
-        double rightBackPower    =  x -y +yaw;
+         DcMotor leftFrontDrive   = null;  //  Used to control the left front drive wheel
+         DcMotor rightFrontDrive  = null;  //  Used to control the right front drive wheel
+         DcMotor leftBackDrive    = null;  //  Used to control the left back drive wheel
+         DcMotor rightBackDrive   = null;  //  Used to control the right back drive wheel
 
-        // Normalize wheel powers to be less than 1.0
-        double max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-        max = Math.max(max, Math.abs(leftBackPower));
-        max = Math.max(max, Math.abs(rightBackPower));
+        leftFrontDrive  = hardwareMap.get(DcMotor.class, "lfm");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rfm");
+        leftBackDrive  = hardwareMap.get(DcMotor.class, "lbm");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rbm");
 
-        if (max > 1.0) {
-            leftFrontPower /= max;
-            rightFrontPower /= max;
-            leftBackPower /= max;
-            rightBackPower /= max;
-        }
+        double l = -gamepad1.left_stick_y; // Remember, Y stick value is reversed
+        double d= gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+        double rx = gamepad1.right_stick_x;
 
-        // Send powers to the wheels.
-        leftFrontDrive.setPower(leftFrontPower);
-        rightFrontDrive.setPower(rightFrontPower);
-        leftBackDrive.setPower(leftBackPower);
-        rightBackDrive.setPower(rightBackPower);
+        double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+        double frontLeftPower = (d + l + rx) / denominator;
+        double backLeftPower = (d - l + rx) / denominator;
+        double frontRightPower = (d - l - rx) / denominator;
+        double backRightPower = (d + l - rx) / denominator;
+
+        leftFrontDrive .setPower(frontLeftPower);
+        leftBackDrive.setPower(backLeftPower);
+        rightFrontDrive.setPower(frontRightPower);
+        rightBackDrive.setPower(backRightPower);
+
     }
 
     /**
