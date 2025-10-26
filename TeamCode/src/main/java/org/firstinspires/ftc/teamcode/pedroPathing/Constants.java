5e61bc92551c328cd50077d06e76238478fcc5d3
Aarav Dhawan <aaravdhawan25@gmail.com>
2025-10-25 23:11:49 -0400

    Update Constants.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -9,6 +9,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants();
+    //mass has to be in kilograms.
+    .mass(5)
 
 
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
