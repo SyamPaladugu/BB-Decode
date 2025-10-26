647275533d2867c2518e023bd1eb62d19fd20f81
Aarav Dhawan <aaravdhawan25@gmail.com>
2025-10-26 19:14:38 -0400

    Update TwoArtifactPP.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TwoArtifactPP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/TwoArtifactPP.java
@@ -5,20 +5,21 @@ import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
+import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.paths.PathChain;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "Pedro Pathing Autonomous", group = " ")
+@Autonomous(name = "2 Artifact PP", group = "Autonomous")
 @Configurable // Panels
-public class Paths extends OpMode {
+public class TwoArtifactPP extends OpMode {
 
     private TelemetryManager panelsTelemetry; // Panels Telemetry instance
     public Follower follower; // Pedro Pathing follower instance
     private int pathState; // Current autonomous path state (state machine)
-    private Paths.paths paths; // Paths defined in the Paths class
+    private Paths paths; // Paths defined in the Paths class
 
     @Override
     public void init() {
@@ -27,7 +28,7 @@ public class Paths extends OpMode {
         follower = Constants.createFollower(hardwareMap);
         follower.setStartingPose(new Pose(72, 8, Math.toRadians(90)));
 
-        paths = new paths(follower); // Build paths
+        paths = new Paths(follower); // Build paths
 
         panelsTelemetry.debug("Status", "Initialized");
         panelsTelemetry.update(telemetry);
@@ -46,21 +47,56 @@ public class Paths extends OpMode {
         panelsTelemetry.update(telemetry);
     }
 
-    public static class paths {
+    public static class Paths {
 
+        public PathChain Path1;
         public PathChain Path2;
+        public PathChain Path3;
+        public PathChain Path4;
 
-        public paths(Follower follower) {
-            Path2 = follower
+        public Paths(Follower follower) {
+            Path1 = follower
                     .pathBuilder()
                     .addPath(
                             new BezierCurve(
                                     new Pose(71.809, 8.043),
-                                    new Pose(72.766, 45.000),
-                                    new Pose(48.447, 95.170)
+                                    new Pose(72.191, 45.000),
+                                    new Pose(50.362, 93.830)
                             )
                     )
                     .setLinearHeadingInterpolation(Math.toRadians(90), Math.toRadians(130))
+                    .build();
+
+            Path2 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierCurve(
+                                    new Pose(50.362, 93.830),
+                                    new Pose(46.915, 83.489),
+                                    new Pose(29.298, 84.255)
+                            )
+                    )
+                    .setTangentHeadingInterpolation()
+                    .build();
+
+            Path3 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierLine(new Pose(29.298, 84.255), new Pose(50.362, 93.638))
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(130))
+                    .build();
+
+            Path4 = follower
+                    .pathBuilder()
+                    .addPath(
+                            new BezierCurve(
+                                    new Pose(50.362, 93.638),
+                                    new Pose(48.830, 55.532),
+                                    new Pose(38.681, 34.085)
+                            )
+                    )
+                    .setConstantHeadingInterpolation(Math.toRadians(90))
                     .build();
         }
     }
