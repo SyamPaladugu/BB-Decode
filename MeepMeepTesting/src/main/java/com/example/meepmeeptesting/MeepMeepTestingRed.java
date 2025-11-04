ec537a3c535a1c300660584aefc8ac07047b65d7
Aarav Dhawan <aaravdhawan25@gmail.com>
2025-11-03 22:04:48 -0500

    ybyhgy

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingRed.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingRed.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class MeepMeepTestingRed{
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -16,18 +16,6 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-
-        //myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(56.75, 5.5, Math.toRadians(55)))
-                // Red Side Goal
-//                  .setTangent(Math.toRadians(45))
-//                .splineToLinearHeading(new Pose2d(-12.2,-27,Math.toRadians(270)), Math.toRadians(0))
-//                .waitSeconds(0.1)
-//                .lineToY(-51.1)
-//                .waitSeconds(0.1)
-//                .lineToY(-27)
-//                .waitSeconds(0.1)
-//                .splineToLinearHeading(new Pose2d(-21.9,-21.9,Math.toRadians(225)),Math.toRadians(0))
-//                .waitSeconds(0.1)
         myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-49.9, -49.7, Math.toRadians(55)))
                 .setTangent(Math.toRadians(45))
                 .splineToLinearHeading(new Pose2d(-12.2,-27,Math.toRadians(270)), Math.toRadians(0))
