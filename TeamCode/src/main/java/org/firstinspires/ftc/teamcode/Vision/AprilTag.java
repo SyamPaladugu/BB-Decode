3e73a5aa06753b46e25a638a38e9dfcdd7729e82
Syam Paladugu <syampaladugu123@gmail.com>
2025-11-22 13:14:20 -0500

    i

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/AprilTag.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import android.util.Size;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.teamcode.subsystem.Subsystem;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AprilTag implements Subsystem {
+private Telemetry telemetry;
+private AprilTagProcessor aprilTagProcessor;
+private VisionPortal visionPortal;
+private  List<AprilTagDetection> detectedTags = new ArrayList<>();
+
+    public void init(HardwareMap map, Telemetry telemetry){
+        this.telemetry = telemetry;
+        aprilTagProcessor = new AprilTagProcessor.Builder()
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .setOutputUnits(DistanceUnit.CM, AngleUnit.DEGREES)
+                .build();
+
+        VisionPortal.Builder builder = new VisionPortal.Builder();
+        builder.setCamera(map.get(WebcamName.class,"Webcam1"));
+        builder.setCameraResolution(new Size(640,480));
+        builder.addProcessor(aprilTagProcessor);
+
+        visionPortal = builder.build();
+    }
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update() {
+        detectedTags = aprilTagProcessor.getDetections();
+    }
+    public List<AprilTagDetection> getDetectedTags(){
+        return detectedTags;
+    }
+
+    public AprilTagDetection getTagByID(int ID){
+        for(AprilTagDetection detection: detectedTags){
+            if(detection.id==ID)
+                return detection;
+        }
+        return null;
+    }
+
+    public void stop(){
+        if(visionPortal != null){
+            visionPortal.close();
+        }
+    }
+    public void displayTelemtry(AprilTagDetection detectedID){
+        if(detectedID == null){return;}
+        if (detectedID.metadata != null) {
+            telemetry.addLine(String.format("\n==== (ID %d) %s", detectedID.id, detectedID.metadata.name));
+            telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detectedID.ftcPose.x, detectedID.ftcPose.y, detectedID.ftcPose.z));
+            telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detectedID.ftcPose.pitch, detectedID.ftcPose.roll, detectedID.ftcPose.yaw));
+            telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detectedID.ftcPose.range, detectedID.ftcPose.bearing, detectedID.ftcPose.elevation));
+        } else {
+            telemetry.addLine(String.format("\n==== (ID %d) Unknown", detectedID.id));
+            telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detectedID.center.x, detectedID.center.y));
+        }
+    }
+
+    public double getDistance(int id){
+        AprilTagDetection tag = getTagByID(id);
+        return tag.ftcPose.range;
+    }
+
+    @Override
+    public void updateCtrls(Gamepad gp1, Gamepad gp2) {
+        detectedTags = aprilTagProcessor.getDetections();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Vision/VisionTest.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.Vision;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+@Disabled
+@TeleOp(name="VisionTest", group = " ")
+public class VisionTest extends OpMode {
+
+    Telemetry telemetry;
+    HardwareMap hardwareMap;
+    AprilTag aprilTag = new AprilTag();
+    @Override
+    public void init(){
+        aprilTag.init(hardwareMap,telemetry);
+
+
+    }
+
+    @Override
+    public void loop() {
+        AprilTagDetection id20 = aprilTag.getTagByID(20);
+        update();
+        aprilTag.displayTelemtry(id20);
+
+
+    }
+
+    public void update(){
+        aprilTag.updateCtrls(gamepad1,gamepad2);
+    }
+}
